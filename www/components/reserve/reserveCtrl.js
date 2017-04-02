c8e9865ea4612d45f0bb31419dea52b5d39607a7
damsu <damorizur@gmail.com>
2017-04-02 17:41:00 +0300

    Now has progress bar with flags and crossing point filled when chosen

--- www/components/reserve/reserveCtrl.js
+++ www/components/reserve/reserveCtrl.js
@@ -4,6 +4,11 @@ app.controller('reserveCtrl', ['$scope', '$http', '$q', '$ionicPopup', 'Borders'
     var destination;
     var crossing;
     var flags = [];
+
+    $scope.progress_flag_left = "blank.png";
+    $scope.progress_flag_right = "blank.png";
+    $scope.progress_crossing = "border_blank.png";
+    $scope.arrow_style = "width: 100%; height: 11px; display: flex; justify-content: flex-start; border-bottom: 2px solid black;";
     $scope.stage = 1;
     Borders.fetchFromServer().then(function(){
         console.log("fetched all borders");
@@ -33,18 +38,24 @@ app.controller('reserveCtrl', ['$scope', '$http', '$q', '$ionicPopup', 'Borders'
         });
         $scope.flags = flags;
         flags = [];
+        $scope.progress_flag_left = Borders.getFlagFrom(from_country);
+        $scope.arrow_style = "width: 100%; height: 11px; display: flex; justify-content: flex-end; border-bottom: 2px solid black;";
 		$scope.stage = 2;
 	}
 
 	$scope.saveDestination = function(to_country){
 		destination = to_country;
 		$scope.addresses = Borders.crossingCities(departure, to_country);
+		$scope.progress_flag_right = Borders.getFlagFrom(to_country);
+		$scope.arrow_style = "width: 100%; height: 11px; display: flex; justify-content: center; border-bottom: 2px solid black;";
 		$scope.stage = 3;
 	}
 
 	$scope.saveCrossing = function(address){
 		crossing = address;
-		$scope.message = "Going from " + departure + " to " + destination + " via " + crossing;
+		$scope.crossing = crossing;
+		$scope.progress_crossing = "border_filled.png";
+		$scope.arrow_style = "display:none;";
 		$scope.stage = 4;
 	}
 }]);
