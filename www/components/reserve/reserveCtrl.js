a166c70599c7ddb0892b4d3d9b09af54363a77a5
damsu <damorizur@gmail.com>
2017-04-09 21:12:32 +0300

    Reservation forms up to stage before confirm

--- www/components/reserve/reserveCtrl.js
+++ www/components/reserve/reserveCtrl.js
@@ -6,6 +6,28 @@ app.controller('reserveCtrl', ['$ionicHistory', '$scope', '$http', '$q', '$ionic
     var flags = [];
     $scope.date = "Date";
     $scope.time = "Time";
+    var image_style_active = "height:35px; width:60px;box-shadow: 0px 0px 20px green;";
+    var image_style_inactive = "height:35px; width:60px;";
+
+    $scope.Person = {
+    	Firstname : null,
+    	Middlename : null,
+    	Lastname : null,
+    	Birthday : null,
+    	Citizenship : null,
+    	Document : null,
+    	DocumentNumber : null,
+    	Email : null,
+    	PhoneNumber : null
+    }
+
+    $scope.Vehicle = {
+    	CarType : null,
+    	CarPlate : null,
+    	Owner : null,
+    	CarManufacturer : null,
+    	CarModel : null
+    }
 
     $scope.init = function(){
     	$ionicHistory.clearCache();
@@ -15,10 +37,13 @@ app.controller('reserveCtrl', ['$ionicHistory', '$scope', '$http', '$q', '$ionic
     	$scope.time = "Time";
     	$scope.fulldatetime = "";
     	$scope.progress_flag_left = "blank.png";
+    	$scope.progress_flag_left_style = image_style_active;
 	    $scope.progress_flag_right = "blank.png";
+	    $scope.progress_flag_right_style = image_style_inactive;
 	    $scope.progress_crossing = "border_blank.png";
+	    $scope.progress_crossing_style = image_style_inactive;
 	    $scope.stage = 0;
-	    $scope.arrow_style = "width: 100%; height: 15px; display: flex; justify-content: flex-start; border-bottom: 2px solid #2a36b9;";
+	    $scope.arrow_style = "width: 100%; height: 15px; display: flex; justify-content: flex-start;";
 	    
 	    Borders.fetchFromServer().then(function(){
 	        console.log("fetched all borders");
@@ -29,7 +54,7 @@ app.controller('reserveCtrl', ['$ionicHistory', '$scope', '$http', '$q', '$ionic
 	        	console.log(value);
 	        	flags.push(Borders.getFlagFrom(value));
 	        	$scope.loading = false;
-	        	$scope.stage = 1; //has to be 1
+	        	$scope.stage = 1; //has to be 1 but can be changed for test purpose
 	        });
 	        $scope.flags = flags;
 	        flags = [];
@@ -54,7 +79,9 @@ app.controller('reserveCtrl', ['$ionicHistory', '$scope', '$http', '$q', '$ionic
         $scope.flags = flags;
         flags = [];
         $scope.progress_flag_left = Borders.getFlagFrom(from_country);
-        $scope.arrow_style = "width: 100%; height: 15px; display: flex; justify-content: flex-end; border-bottom: 2px solid #2a36b9;";
+        $scope.progress_flag_left_style = image_style_inactive;
+	    $scope.progress_flag_right_style = image_style_active;
+	    $scope.progress_crossing_style = image_style_inactive;
 		$scope.stage = 2;
 	}
 
@@ -62,7 +89,9 @@ app.controller('reserveCtrl', ['$ionicHistory', '$scope', '$http', '$q', '$ionic
 		destination = to_country;
 		$scope.addresses = Borders.crossingCities(departure, to_country);
 		$scope.progress_flag_right = Borders.getFlagFrom(to_country);
-		$scope.arrow_style = "width: 100%; height: 15px; display: flex; justify-content: center; border-bottom: 2px solid #2a36b9;";
+		$scope.progress_flag_left_style = image_style_inactive;
+	    $scope.progress_flag_right_style = image_style_inactive;
+	    $scope.progress_crossing_style = image_style_active;
 		$scope.stage = 3;
 	}
 
@@ -70,7 +99,9 @@ app.controller('reserveCtrl', ['$ionicHistory', '$scope', '$http', '$q', '$ionic
 		crossing = address;
 		$scope.crossing = crossing;
 		$scope.progress_crossing = "border_filled.png";
-		$scope.arrow_style = "display:none;";
+		$scope.progress_flag_left_style = image_style_inactive;
+	    $scope.progress_flag_right_style = image_style_inactive;
+	    $scope.progress_crossing_style = image_style_inactive;
 		$scope.stage = 4;
 	}
 
@@ -146,12 +177,38 @@ app.controller('reserveCtrl', ['$ionicHistory', '$scope', '$http', '$q', '$ionic
   	}
   };
 
+  $scope.toVehicleInfo = function(){
+  	$scope.stage = 6;
+  };
+
+  $scope.toConfirmView = function(){
+  	$scope.stage = 7;
+  };
+
+  var ipObj3 = {
+      callback: function (val) {  //Mandatory 
+        console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+        var birthday = new Date(val);
+        $scope.Person.Birthday = birthday.toDateString();
+      },
+      from: new Date(1900, 1, 1), //Optional 
+      to: new Date(), //Optional 
+      inputDate: new Date(),      //Optional 
+      mondayFirst: true,          //Optional 
+      closeOnSelect: true,       //Optional 
+      templateType: 'popup'       //Optional 
+    };
+ 
+    $scope.openBirthdayPicker = function(){
+      ionicDatePicker.openDatePicker(ipObj3);
+    };
+
   function addZero(i) {
     if (i < 10) {
         i = "0" + i;
     }
     return i;
-}
+  }
   
 
 
