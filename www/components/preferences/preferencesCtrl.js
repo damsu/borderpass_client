53c9d714e26797b4407fe0bbf7bcec94acd9ca90
damsu <damorizur@gmail.com>
2017-04-18 13:37:23 +0300

    clear all cache in one button + explanation

--- www/components/preferences/preferencesCtrl.js
+++ www/components/preferences/preferencesCtrl.js
@@ -2,29 +2,20 @@ app.controller('preferencesCtrl', ["$scope", function ($scope) {
 
 
 	$scope.init = function(){
-		if (window.localStorage.hasOwnProperty("Person")||window.localStorage.hasOwnProperty("Vehicle")){
-	  		$scope.pers_data_deletable = false;
+		if (window.localStorage.hasOwnProperty("Person")||window.localStorage.hasOwnProperty("Vehicle")||window.localStorage.hasOwnProperty("ReservationIDs")){
+	  		$scope.no_data = false;
   		} else {
-	  		$scope.pers_data_deletable = true;
+	  		$scope.no_data = true;
   		}
 
-  		if (window.localStorage.hasOwnProperty("ReservationIDs")){
-	  		$scope.res_nums_deletable = false;
-  		} else {
-	  		$scope.res_nums_deletable = true;
-  		}
   		console.log("Person : " + window.localStorage.getItem("Person"));
   		console.log("Vehicle : " + window.localStorage.getItem("Vehicle"));
   		console.log("ReservationIDs : " + window.localStorage.getItem("ReservationIDs"));
 	}
 
-	$scope.deletePersonData = function(){
+	$scope.deleteData = function(){
 		window.localStorage.removeItem("Person");
 		window.localStorage.removeItem("Vehicle");
-		$scope.init();
-	}
-
-	$scope.deleteResNums = function(){
 		window.localStorage.removeItem("ReservationIDs");
 		$scope.init();
 	}
